44f1dc4a86310c0fba8271d3224e47ff24cbea20
Jan Schüler <jandeluxe96@gmail.com>
2025-07-30 11:36:09 +0200

    d

--- remove_empty_link/plugin.py
+++ remove_empty_link/plugin.py
@@ -6,7 +6,7 @@ class RemoveEmptyLinkPlugin(InvenTreePlugin):
     SLUG = "remove-empty-link"
     TITLE = "Leere Links entfernen"
     VERSION = "1.0"
-    DESCRIPTION = "Verhindert Fehler bei leeren link-Feldern"
+    DESCRIPTION = "Plugin entfernt leere Strings im Feld 'link', um Fehler zu verhindern."
 
     def pre_save_part(self, sender, instance: Part, data: dict, user, **kwargs):
         if data.get("link", "").strip() == "":
